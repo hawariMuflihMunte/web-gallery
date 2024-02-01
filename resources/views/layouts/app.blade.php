dec2ec987967036f7f3ea5f8c2dc9c3168b13544
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-02-01 14:49:07 +0700

    feat: create view `layouts.app`

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    @vite('resources/css/app.css')
+
+    @yield('styles')
+    @show
 </head>
 <body>
     @yield('content')
