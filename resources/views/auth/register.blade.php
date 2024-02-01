82cf8ea9a1decafbf9e8d5eb46581a6ebc7a8ff7
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-02-01 14:49:26 +0700

    feat: create view `auth.register`

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
+
 @section('title', 'Web Gallery Register')
+
+@section('styles')
+<style>
+main {
+    background-image: url('./images/bg_register.png');
+    background-size: 100%;
+}
+</style>
+@endsection
+
 @section('content')
-    
+    <main class="min-w-full min-h-full h-[100vh] flex items-center justify-center">
+        <section class="glass_morphism max-w-96">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur atque ipsam exercitationem quibusdam veritatis culpa repellat dolorem nemo quod eaque deserunt accusantium itaque, modi sit voluptates asperiores! Nihil, quidem quos.
+        </section>
+    </main>
 @endsection
