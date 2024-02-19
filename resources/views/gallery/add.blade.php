fe3de3dab8ee52f8e7abe06f77337d6e05dc1733
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-02-19 15:25:22 +0700

    feat(ui): generate inputs based on image counter

--- resources/views/gallery/add.blade.php
+++ resources/views/gallery/add.blade.php
@@ -23,24 +23,82 @@
                     reader.onload = e => callback(e.target.result);
                 }
             }"
-            class="bg-red-200 p-2"
+            class="px-5 py-3 max-w-[92%] md:max-w-[80%] mx-auto flex flex-col gap-4"
         >
-            <input
-                type="file"
-                name="image"
-                id="image"
-                accept="image/*"
-                @change="fileChosen"
-            />
-            <template x-if="imageUrl">
-                <section >
-                    <img
-                        :src="imageUrl"
-                        :alt="imageUrl"
-                        class="w-60 h-32"
-                    />
-                </section>
-            </template>
+            <section>
+                <a
+                    href="{{ route('gallery.index') }}"
+                    class="glass_morphism_navy_bg px-2 py-1"
+                >
+                    <i class="bi-arrow-left-short"></i>
+                    Kembali
+                </a>
+            </section>
+            <section>
+                <form
+                    action="{{ route('gallery.store') }}"
+                    method="post"
+                    enctype="multipart/form-data"
+                    class="bg-lime-200 px-5 py-6 rounded-sm border border-slate-300 shadow-md"
+                    x-data="{
+                        imageCount: 1,
+                        incrementImageCount() {
+                            if (this.imageCount < 20) {
+                                this.imageCount++;
+                            } else {
+                                this.imageCount = 20;
+                            }
+                        },
+                        decrementImageCount() {
+                            if (this.imageCount > 1) {
+                                this.imageCount--;
+                            } else {
+                                this.imageCount = 1;
+                            }
+                        }
+                    }"
+                >
+                    <h1 class="text-2xl font-medium">Tambah Album</h1>
+                    <hr class="my-3 border-slate-500">
+                    <h2>Gambar (<span x-text="imageCount"></span>)</h2>
+                    <button
+                        type="button"
+                        @click="incrementImageCount"
+                    >
+                        <i class="bi-plus-square-fill"></i>
+                    </button>
+                    <button
+                        type="button"
+                        @click="decrementImageCount"
+                    >
+                        <i class="bi-dash-square-fill"></i>
+                    </button>
+                    <template x-for="count in imageCount">
+                        <article
+                            class="flex items-center gap-3"
+                        >
+                            <h3 x-text="count"></h3>
+                            <input
+                                type="file"
+                                name="image"
+                                id="image"
+                                accept="image/*"
+                                @change="fileChosen"
+                                class=""
+                            />
+                        </article>
+                    </template>
+                    <template x-if="imageUrl">
+                        <section>
+                            <img
+                                :src="imageUrl"
+                                :alt="imageUrl"
+                                class="w-60 h-32"
+                            />
+                        </section>
+                    </template>
+                </form>
+            </section>
         </section>
     </main>
 
