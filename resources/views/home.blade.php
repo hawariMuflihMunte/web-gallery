78d7dd8f84c9cd4473e46611c12570936c807a52
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-02-20 14:26:45 +0700

    feat: display album from DB based on current user id

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,6 +37,31 @@
           <h2>Tambah Album</h2>
         </section>
       </a>
+      @foreach ($albums as $album)
+        <a
+          href="{{ route('gallery.edit', $album['AlbumID']) }}"
+          class="flex flex-col bg-emerald-300 text-slate-700 font-bold rounded-sm max-w-[200px] md:max-w-[240px] lg:min-w-[300px] overflow-hidden cursor-pointer border border-slate-400"
+          role="article"
+          x-data="{
+            showImage: false
+          }"
+          x-on:mouseover="showImage = true"
+          x-on:mouseleave="showImage = false"
+        >
+          <section class="w-full">
+            <img
+              src="{{ asset('images/bg_add_image.png') }}"
+              alt="illustration"
+              class="h-auto w-full"
+              x-show="showImage"
+              x-transition.duration.200ms
+            />
+          </section>
+          <section class="w-full text-inherit min-w-[200px] md:min-w-[240px] lg:min-w-[300px] flex justify-center items-center py-4">
+            <h2>{{ $album['NamaAlbum'] }}</h2>
+          </section>
+        </a>
+      @endforeach
     </section>
   </main>
 
