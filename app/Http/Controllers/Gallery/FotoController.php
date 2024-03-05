1f466b9ffd201a8662f3be589fe20f9362c0fbee
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-03-05 22:48:10 +0700

    refactor(controller): move `FotoController` to `Gallery/`

--- app/Http/Controllers/Gallery/FotoController.php
+++ app/Http/Controllers/Gallery/FotoController.php
@@ -1,18 +1,19 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Gallery;
 
+use App\Http\Controllers\Controller;
 use App\Models\Album;
 use App\Models\Foto;
-use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\View\View;
 
 class FotoController extends Controller
 {
-  public function __construct()
+    public function __construct()
   {
     $this->middleware("auth");
   }
