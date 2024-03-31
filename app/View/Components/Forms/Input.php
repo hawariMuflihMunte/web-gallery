432b2cb1e13741473c8c529164d0e0a43ed095d3
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-03-31 23:07:44 +0700

    component: Forms/Input

---
+++ app/View/Components/Forms/Input.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\View\Components\Forms;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Input extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public string $type = 'text',
+        public string $placeholder = '',
+        public string $value = '',
+    ) { }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.forms.input');
+    }
+}
