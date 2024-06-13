b230ae111840ec6a467bd63cb23b940b487cdd3a
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-06-13 21:18:09 +0700

    feat(factory): `User`

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -24,11 +23,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'Username' => fake()->userName(),
+            'Password' => Hash::make('12345678'),
+            'Email' => fake()->unique()->safeEmail(),
+            'NamaLengkap' => fake()->name(),
+            "Alamat" => fake()->address(),
         ];
     }
 
