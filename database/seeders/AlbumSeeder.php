29f78b0f54d788555155bbec9a1e8f01c07696ca
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-06-13 21:32:47 +0700

    feat: create seeder for album and user

---
+++ database/seeders/AlbumSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Album;
+use Illuminate\Database\Seeder;
+
+class AlbumSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Album::factory(10)->create();
+    }
+}
