03f5966a8ba54c29b1994f9e4757416f807ecfcb
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-06-14 00:38:41 +0700

    refactor: update nav content and styling

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -26,26 +26,26 @@
         <template x-if="openList">
             <section class="relative">
                 <section
-                    class="absolute right-0 top-full rounded-sm border-slate-300 bg-white shadow"
+                    class="absolute right-0 top-full rounded-sm border-slate-300 bg-white shadow min-w-[180px]"
                 >
                     <ul class="flex flex-col">
                         <li>
                             <a
                                 href="{{ route("profile.index") }}"
-                                class="flex items-center gap-2 bg-inherit p-3 text-slate-800 transition-all duration-100 hover:bg-emerald-400"
+                                class="flex content-between align-center gap-2 bg-inherit p-3 text-slate-800 transition-all duration-100 hover:bg-emerald-400 w-full"
                             >
                                 <i class="bi-person-circle"></i>
-                                <span class="ps-3">Profil</span>
+                                <span class="w-full ps-3 text-right">@lang('app.profile')</span>
                             </a>
                         </li>
                         <li>
                             <button
                                 type="button"
-                                class="flex items-center gap-2 p-3 transition-all duration-100 hover:bg-red-500 hover:text-white"
+                                class="flex content-between align-center gap-2 p-3 transition-all duration-100 hover:bg-red-500 hover:text-white w-full"
                                 @click="logoutConfirm = true"
                             >
                                 <i class="bi-x-square"></i>
-                                <span class="ps-3">Keluar</span>
+                                <span class="w-full ps-3 text-right">@lang('app.logout')</span>
                             </button>
                         </li>
                     </ul>
@@ -63,7 +63,7 @@
             class="glass_morphism_bg md:1/2 w-full rounded-sm px-8 py-10"
             @click.outside="logoutConfirm = false"
         >
-            <h3 class="text-2xl">Yakin ingin keluar ?</h3>
+            <h3 class="text-2xl">@lang('app.confirm_logout')</h3>
             <hr class="my-2 border-gray-700" />
             <section class="my-6"></section>
             <section class="flex gap-6">
@@ -71,13 +71,13 @@
                     href="{{ route("logout") }}"
                     class="glass_morphism_red_bg rounded-md px-3 py-1"
                 >
-                    Ya
+                  @lang('app.yes')
                 </a>
                 <button
                     class="glass_morphism_navy_bg rounded-md px-3 py-1"
                     @click="logoutConfirm = false"
                 >
-                    Batal
+                    @lang('app.no')
                 </button>
             </section>
         </article>
