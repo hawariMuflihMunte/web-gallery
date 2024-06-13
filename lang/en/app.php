1387257a874075569651b1a92d6d31c430a9acbc
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-06-14 00:18:33 +0700

    feat: string localization asset

---
+++ lang/en/app.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+  'albums' => 'Albums',
+  'album' => 'Album',
+  'your_album' => 'Your Album',
+  'view_all_albums' => 'View All Albums',
+  'create_new_album' => 'Create New Album',
+  'upload_images' => 'Upload Images',
+  'image_gallery' => 'Image Gallery',
+  'image' => 'Image',
+  'add_image' => 'Add Image',
+  'edit_image' => 'Edit Image',
+  'delete_image' => 'Delete Image',
+  'signup' => 'Sign Up',
+  'login' => 'Log In',
+  'logout' => 'Log Out',
+  'yes' => 'Yes',
+  'no' => 'No',
+  'proceed' => 'Proceed',
+  'cancel' => 'Cancel',
+  'users' => 'Users',
+  'user' => 'User',
+  'profile' => 'Profile'
+];
