43d69e592f7b1c4e4eb5341fa8b36ce2268984c5
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-06-13 21:30:34 +0700

    feat(factory): album factory

---
+++ database/factories/AlbumFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Album>
+ */
+class AlbumFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $user = User::all();
+        $data = [];
+
+        foreach ($user as $key => $value) {
+            array_push($data, $value->UserID);
+        }
+
+        return [
+            "NamaAlbum" => fake()->name(),
+            "DeskripsiAlbum" => fake()->text(100),
+            "TanggalDibuat" => fake()->date(),
+            "UserID" => fake()->randomElement($data),
+        ];
+    }
+}
