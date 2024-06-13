bd674d4248e1902e8c73a459ee486bf32adf8df8
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-06-13 21:17:56 +0700

    refactor(model): set $guarded for `User` data

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,18 +14,7 @@ class User extends Authenticatable
     protected $primaryKey = "UserID";
     public $timestamps = false;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        "Username",
-        "Password",
-        "Email",
-        "NamaLengkap",
-        "Alamat",
-    ];
+    protected $guarded = ['UserID'];
 
     /**
      * The attributes that should be hidden for serialization.
