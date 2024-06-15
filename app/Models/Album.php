726087326a77222e0205cae5431de57e5daf2016
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-06-16 01:52:27 +0700

    fix: update model and link it to its factory

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Album extends Model
 {
+    use HasFactory;
+
     protected $table = "album";
     protected $primaryKey = "AlbumID";
     public $timestamps = false;
