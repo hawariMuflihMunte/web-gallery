546822423dea85bc62d04083a5b4f4f30c4ca441
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-06-16 20:59:23 +0700

    refactor: update nav bar style - add lang string

--- resources/views/app/gallery/index.blade.php
+++ resources/views/app/gallery/index.blade.php
@@ -31,7 +31,7 @@
             <h1 class="mb-12 text-center text-2xl font-semibold sm:text-left">Albums</h1>
             <section
                 role="group"
-                class="lg:scrollbar-w-2 lg:scrollbar-track-rounded lg:scrollbar-thumb-rounded lg:scrollbar-color-emerald-400 lg:scrollbar-thumb-emerald-400 grid max-h-[40vh] grid-cols-1 gap-5 overflow-y-scroll sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+                class="lg:scrollbar-w-2 lg:scrollbar-track-rounded lg:scrollbar-thumb-rounded lg:scrollbar-color-emerald-400 lg:scrollbar-thumb-emerald-400 grid max-h-[60vh] grid-cols-1 gap-5 overflow-y-scroll sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
             >
                 @foreach ($albums as $album)
                     <a
@@ -59,64 +59,6 @@
                         </section>
                     </a>
                 @endforeach
-            </section>
-
-            <section class="mt-40">
-                <h1 class="mb-12 text-2xl font-semibold">@lang("app.your_album")</h1>
-                <section
-                    role="group"
-                    class="lg:scrollbar-w-2 lg:scrollbar-track-rounded lg:scrollbar-thumb-rounded lg:scrollbar-color-emerald-400 lg:scrollbar-thumb-emerald-400 grid max-h-[40vh] grid-cols-1 gap-5 overflow-y-scroll sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
-                >
-                    <a
-                        href="{{ route("gallery.create") }}"
-                        class="relative flex h-full cursor-pointer flex-col overflow-hidden rounded-sm border border-slate-400 bg-slate-200 font-bold text-slate-700"
-                        role="article"
-                    >
-                        <section class="relative h-full w-full overflow-hidden">
-                            <img
-                                src="{{ asset("images/bg_add_image.png") }}"
-                                alt="illustration"
-                                class="h-full w-full object-cover"
-                                loading="lazy"
-                            />
-                            <section
-                                class="absolute left-0 top-0 z-10 flex h-full w-full flex-col items-center justify-center text-center text-lg tracking-widest text-transparent duration-200 ease-linear hover:bg-white hover:bg-opacity-95 hover:text-slate-800"
-                            >
-                                <h2 class="text-center">
-                                    @lang("app.create_new_album")
-                                </h2>
-                            </section>
-                        </section>
-                    </a>
-                    @foreach ($user->album()->get() as $album)
-                        <a
-                            href="{{ route("gallery.edit", $album["AlbumID"]) }}"
-                            class="relative flex h-full cursor-pointer flex-col overflow-hidden rounded-sm border border-slate-400 bg-slate-200 font-bold text-slate-700"
-                            role="article"
-                        >
-                            <section class="relative h-full w-full overflow-hidden">
-                                <img
-                                    @if (! empty($album->foto()->get()->first()))
-                                        src="{{ url("/storage/" .$album->foto()->get()->first()["LokasiFile"],) }}"
-                                        alt="{{ $album->foto()->get()->first()["DeskripsiFoto"] }}"
-                                    @else
-                                        src="{{ asset("images/bg_no_image.png") }}"
-                                        alt="Coffee Time. There are no images in this album"
-                                    @endif
-                                    class="h-full w-full object-cover"
-                                    loading="lazy"
-                                />
-                                <section
-                                    class="absolute left-0 top-0 z-10 flex h-full w-full flex-col items-center justify-center text-center text-lg tracking-widest text-transparent duration-200 ease-linear hover:bg-white hover:bg-opacity-95 hover:text-slate-800"
-                                >
-                                    <h2 class="text-center">
-                                        {{ $album["NamaAlbum"] }}
-                                    </h2>
-                                </section>
-                            </section>
-                        </a>
-                    @endforeach
-                </section>
             </section>
         </section>
     </main>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,5 +1,5 @@
 <nav
-    class="fixed z-50 flex min-h-10 w-full items-center justify-between bg-emerald-200 px-5 py-3 text-gray-700 shadow"
+    class="fixed z-50 flex min-h-10 w-full items-center justify-between bg-teal-50 p-0 text-gray-700 shadow"
     x-data="{
         openList: false,
         logoutConfirm: false,
@@ -14,48 +14,77 @@
             alt="Web Gallery"
         />
     </a>
-    <section class="flex">
-        <button
-            type="button"
-            class="rounded-sm bg-white px-3 py-1 text-lg outline-none transition duration-100 hover:opacity-80"
-            @click="openList = !openList"
-            @click.outside="openList = false"
-        >
-            <i class="bi-list"></i>
-        </button>
-        <template x-if="openList">
-            <section class="relative">
-                <section
-                    class="absolute right-0 top-full min-w-[180px] rounded-sm border-slate-300 bg-white shadow"
-                >
-                    <ul class="flex flex-col">
-                        <li>
-                            <a
-                                href="{{ route("profile.index") }}"
-                                class="align-center flex w-full content-between gap-2 bg-inherit p-3 text-slate-800 transition-all duration-100 hover:bg-emerald-400"
-                            >
-                                <i class="bi-person-circle"></i>
+    <section class="flex items-center m-0 p-0 gap-4">
+        <h1>
+            <i class="bi-person-fill"></i>
+            {{ auth()->user()->Username }} @role('admin') (@lang('app.administrator')) @endrole
+        </h1>
+        <section class="flex items-center m-0 p-0 border-l-2 border-l-slate-400">
+            <button
+                type="button"
+                class="rounded-sm bg-inherit px-3 py-1 m-0 outline-none transition duration-100 hover:opacity-80 flex place-content-center place-items-center"
+                @click="openList = !openList"
+                @click.outside="openList = false"
+            >
+                <i x-show="openList" class="bi-x-lg text-2xl"></i>
+                <i x-show="!openList" class="bi-list text-2xl"></i>
+            </button>
+            <template x-if="openList">
+                <section class="relative top-[20px]">
+                    <section
+                        class="absolute right-0 top-full min-w-[180px] rounded-sm border-slate-300 bg-white shadow"
+                    >
+                        <ul class="flex flex-col">
+                            <li>
+                                <a
+                                    href="{{ route("gallery.index") }}"
+                                    class="align-center flex w-full content-between gap-2 bg-inherit p-3 text-slate-800 transition-all duration-100 hover:bg-emerald-400"
+                                >
+                                    <i class="bi-house"></i>
+                                    <span class="w-full ps-3 text-right">
+                                        @lang("app.home")
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="{{ route("album.index") }}"
+                                    class="align-center flex w-full content-between gap-2 bg-inherit p-3 text-slate-800 transition-all duration-100 hover:bg-emerald-400"
+                                >
+                                <i class="bi-ui-checks-grid"></i>
                                 <span class="w-full ps-3 text-right">
-                                    @lang("app.profile")
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <button
-                                type="button"
-                                class="align-center flex w-full content-between gap-2 p-3 transition-all duration-100 hover:bg-red-500 hover:text-white"
-                                @click="logoutConfirm = true"
-                            >
-                                <i class="bi-x-square"></i>
-                                <span class="w-full ps-3 text-right">
-                                    @lang("app.logout")
-                                </span>
-                            </button>
-                        </li>
-                    </ul>
+                                        @lang("app.your_albums")
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="{{ route("profile.index") }}"
+                                    class="align-center flex w-full content-between gap-2 bg-inherit p-3 text-slate-800 transition-all duration-100 hover:bg-emerald-400"
+                                >
+                                    <i class="bi-person-circle"></i>
+                                    <span class="w-full ps-3 text-right">
+                                        @lang("app.profile")
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <button
+                                    type="button"
+                                    class="align-center flex w-full content-between gap-2 p-3 transition-all duration-100 hover:bg-red-500 hover:text-white"
+                                    @click="logoutConfirm = true"
+                                >
+                                    <i class="bi-x-square"></i>
+                                    <span class="w-full ps-3 text-right">
+                                        @lang("app.logout")
+                                    </span>
+                                </button>
+                            </li>
+                        </ul>
+                    </section>
                 </section>
-            </section>
-        </template>
+            </template>
+        </section>
     </section>
     {{-- Popups --}}
     <section
