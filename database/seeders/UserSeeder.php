39098a9e901fea3ab61747acbc4c4b06b8296b94
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-06-18 16:40:23 +0700

    feat(seeder): add User and Administrator and set roles for each user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -13,18 +14,34 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        DB::table('user')->delete();
+
         $users = User::factory(10)->create();
 
         foreach ($users as $user) {
             $user->assignRole('user');
         }
+        
+        // Create User only if it doesn't already exist
+        if (!User::where('Email', 'user@user.net')->exists()) {
+            User::create([
+                'Username' => 'sser',
+                'Password' => Hash::make('12345678'),
+                'Email' => 'user@user.net',
+                'NamaLengkap' => 'User',
+                'Alamat' => '-',
+            ])->assignRole('user');
+        }
 
-        User::create([
-            'Username' => "administrator",
-            'Password' => Hash::make('administrator'),
-            'Email' => "admin@admin.com",
-            'NamaLengkap' => "Administrator",
-            "Alamat" => "-",
-        ])->assignRole('admin');
+        // Create administrator only if it doesn't already exist
+        if (!User::where('Email', 'admin@admin.com')->exists()) {
+            User::create([
+                'Username' => 'administrator',
+                'Password' => Hash::make('administrator'),
+                'Email' => 'admin@admin.com',
+                'NamaLengkap' => 'Administrator',
+                'Alamat' => '-',
+            ])->assignRole('admin');
+        }
     }
 }
