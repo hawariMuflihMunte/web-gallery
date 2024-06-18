c71443e5ec28f0a242a6291c79a9dfb04d922c07
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-06-18 22:25:36 +0700

    feat(component): create `Layouts/Modal` component

---
+++ app/View/Components/Layouts/Modal.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components\Layouts;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Modal extends Component
+{
+    public ?string $modalId;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(string $modalId = null)
+    {
+        $this->modalId = $modalId;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.layouts.modal', [
+            'modalId' => $this->modalId,
+        ]);
+    }
+}
