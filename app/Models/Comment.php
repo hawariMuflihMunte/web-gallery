d3ef22daa80d31cd1a4df0986e8c1a55979f1595
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-07-04 22:44:13 +0700

    fix(model): generate slug in `Comment` model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,16 +5,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Comment extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
 
     protected $table = "comments";
     protected $primaryKey = "id";
     public $timestamps = true;
 
     protected $guarded = ["id"];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom("created_at")
+            ->saveSlugsTo("slug");
+    }
+
+    public function getRouteKeyName(): string
+    {
+        return "slug";
+    }
 
     public function photo(): BelongsTo
     {
