674a97db78cf74fdc3914b2c48138640f276c577
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-07-04 22:24:59 +0700

    fix(model): implement 2/2 todos in `Comment` model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -12,24 +12,17 @@ class Comment extends Model
 
     protected $table = "comments";
     protected $primaryKey = "id";
-    public $timestamps = false;
+    public $timestamps = true;
 
     protected $guarded = ["id"];
 
-
-    /** TODO: [PHOTO] Implement photo model relationship
-     *
-     * - Update photo model relatiionship key
-     * - Update user model relatiionship key
-     * */
-
     public function photo(): BelongsTo
     {
-        return $this->belongsTo(Foto::class, "FotoID");
+        return $this->belongsTo(Photo::class, 'id', 'photo_id');
     }
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class, "UserID");
+        return $this->belongsTo(User::class, 'id', 'user_id');
     }
 }
