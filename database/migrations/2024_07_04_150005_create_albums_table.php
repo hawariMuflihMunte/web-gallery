efad9c756e6f028d83caba2d8872be737389c38b
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-07-04 22:52:44 +0700

    fix(model): typo in albums relation

---
+++ database/migrations/2024_07_04_150005_create_albums_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title', 128);
+            $table->text('description');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('albums');
+    }
+};
